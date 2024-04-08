257cf2349bd2a5a97fd0520e164070b0d1e0bb91
AriannaAguilarLeiva <arianna.aguilar.leiva@est.una.ac.cr>
2024-04-08 11:35:00 -0600

    Mejoré el estilo para poder ver 3 noticias por fila

--- my-app/src/app/components/Dashboard.tsx
+++ my-app/src/app/components/Dashboard.tsx
@@ -20,32 +20,26 @@ const Dashboard = () => {
     };
     fetchNews();
   }, []);
-  
+
   return (
-    
     <section className="text-gray-400 body-font bg-gray-900">
-      
       <div className="container px-5 py-24 mx-auto">
         <div className="flex flex-wrap w-full mb-20 flex-col items-center text-center">
           <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-white">Las últimas 12 noticias</h1>
           <p className="lg:w-1/2 w-full leading-relaxed text-opacity-80">Las mejores noticias en Costa Rica</p>
         </div>
-        <div className="flex flex-wrap -m-4">
-          <ul>
-
-          
+        <div className="flex flex-wrap -mx-4">
           {news.map((article: any, index: number) => (
-            <li key={index}>
-             <NewItem author={article.author} title={article.title}/>
-            </li>
+            <div key={index} className="w-full sm:w-1/2 md:w-1/3 lg:w-1/3 xl:w-1/3 px-4 mb-8">
+              <NewItem author={article.author} title={article.title}/>
+            </div>
           ))}
-          </ul>
         </div>
         <button className="flex mx-auto mt-16 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
       </div>
     </section>
-
   );
+  
 };
 
 export default Dashboard;
--- my-app/src/app/components/NewItem .tsx
+++ my-app/src/app/components/NewItem .tsx
@@ -8,18 +8,15 @@ interface NewItemProps{
 
 const NewItem: React.FC<NewItemProps> = ({author, title}) => {
   return (
-    <div className="xl:w-1/3 md:w-1/2 p-4">
-      <div className="border border-gray-700 border-opacity-75 p-6 rounded-lg">
-        <div className="w-10 h-10 inline-flex items-center justify-center rounded-full bg-gray-800 text-indigo-400 mb-4">
-
-        <SmallPicture></SmallPicture>
-
-        </div>
-        <h2 className="text-lg text-white font-medium title-font mb-2">{author}</h2>
-        <p className="leading-relaxed text-base">{title}</p>
+    <div className="border border-gray-700 border-opacity-75 p-6 rounded-lg">
+      <div className="w-10 h-10 inline-flex items-center justify-center rounded-full bg-gray-800 text-indigo-400 mb-4">
+        <SmallPicture />
       </div>
+      <h2 className="text-lg text-white font-medium title-font mb-2">{author}</h2>
+      <p className="leading-relaxed text-base">{title}</p>
     </div>
   );
 };
+
 
 export default NewItem;
